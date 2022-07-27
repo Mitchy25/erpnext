02c119d7b86a65cb2e24a9f15f0d90d8827eb826
Mitch Stokes <mitch@fxmed.co.nz>
2022-07-28 11:25:51 +1200

    Set default group_by

--- erpnext/accounts/report/item_wise_sales_register/item_wise_sales_register.js
+++ erpnext/accounts/report/item_wise_sales_register/item_wise_sales_register.js
@@ -59,6 +59,7 @@ frappe.query_reports["Item-wise Sales Register"] = {
 			"fieldname": "group_by",
 			"fieldtype": "Select",
 			"options": ["Customer Group", "Customer", "Item Group", "Item", "Territory", "Invoice"],
+			"default": "Item",
 			"reqd": 1
 		}
 	],
