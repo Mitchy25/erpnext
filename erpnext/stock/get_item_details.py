157581cc3443f455db4e08683e76eba22d2e5ba9
Stan Lukuvka <stanluku@gmail.co.nz>
2024-01-23 11:51:50 +1300

    fix: indentation

--- erpnext/stock/get_item_details.py
+++ erpnext/stock/get_item_details.py
@@ -117,7 +117,7 @@ def get_item_details(args, doc=None, for_validate=False, overwrite_warehouse=Tru
 	if (args.has_batch_no and args.batch_no) or not args.has_batch_no or args.get("update_stock"):
 		data = get_pricing_rule_for_item(args, out.price_list_rate, doc, for_validate=for_validate)
 
-	out.update(data)
+		out.update(data)
 
 	update_stock(args, out)
 
