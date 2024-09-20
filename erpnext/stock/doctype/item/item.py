2094112912823a70663177423378a5c5966d4071
Tom Fretwell <Tom@Fxmed.co.nz>
2024-09-20 13:30:06 +1200

    Fix: cleaning

--- erpnext/stock/doctype/item/item.py
+++ erpnext/stock/doctype/item/item.py
@@ -1294,7 +1294,6 @@ def validate_item_default_company_links(item_defaults: List[ItemDefault]) -> Non
 						title=_("Invalid Item Defaults"),
 					)
 
-
 @frappe.whitelist()
 def get_asset_naming_series():
 	from erpnext.assets.doctype.asset.asset import get_asset_naming_series
