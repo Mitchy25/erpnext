1168342dd736db5540be45cd5e2c5c594e210945
Tom Fretwell <Tom@Fxmed.co.nz>
2024-10-17 13:40:44 +1300

    Fix: from and to date defaults + reqd

--- erpnext/crm/report/sales_pipeline_analytics/sales_pipeline_analytics.js
+++ erpnext/crm/report/sales_pipeline_analytics/sales_pipeline_analytics.js
@@ -14,11 +14,16 @@ frappe.query_reports["Sales Pipeline Analytics"] = {
 			fieldname: "from_date",
 			label: __("From Date"),
 			fieldtype: "Date",
+			default: frappe.datetime.add_months(frappe.datetime.get_today(), -6),
+			width: "80",
+			reqd:1
 		},
 		{
 			fieldname: "to_date",
 			label: __("To Date"),
 			fieldtype: "Date",
+			default: frappe.datetime.get_today(),
+			reqd:1
 		},
 		{
 			fieldname: "range",
