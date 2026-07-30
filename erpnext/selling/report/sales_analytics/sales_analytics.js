06a8bc713facaadfdb5416f719a80e534de50f71
tom <tom@fxmed.co.nz>
2026-07-31 11:34:08 +1200

    Fix: Hidding company ifeld to prevent confusion

--- erpnext/selling/report/sales_analytics/sales_analytics.js
+++ erpnext/selling/report/sales_analytics/sales_analytics.js
@@ -91,6 +91,7 @@ frappe.query_reports["Sales Analytics"] = {
 			fieldname: "show_aggregate_value_from_subsidiary_companies",
 			label: __("Show Aggregate Value from Subsidiary Companies"),
 			fieldtype: "Check",
+			hidden: 1
 		},
 	],
 	get_datatable_options(options) {
